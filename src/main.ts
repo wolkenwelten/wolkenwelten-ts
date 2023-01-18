d1cfe8f4219b505dbd281dd26830cec95b051dc6
Ben (Xeon/Arch) <ben@wolkenwelten.net>
2023-01-18 03:16:04 +0100

    Added `window.wolkenwelten` for debugging.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import './style.css';
 import { Game } from './game';
 
+declare global {
+    interface Window {
+        wolkenwelten: Game;
+    }
+}
+
 const main = () => {
     const parent = document.querySelector('#wolkenwelten-parent');
     if (!parent) {
         throw new Error("Can't find WolkenWelten parent element");
     }
-    new Game({
+    const game = new Game({
         parent: parent as HTMLElement,
     });
+    window.wolkenwelten = game;
 };
 setTimeout(main, 0);
