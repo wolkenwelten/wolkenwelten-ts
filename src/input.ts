c73f03762c72b66db9ec658d39b97fa97750fcda
Ben (X13/Arch) <ben@wolkenwelten.net>
2023-02-09 18:02:13 +0100

    GamePad support

--- src/input.ts
+++ src/input.ts
@@ -5,6 +5,8 @@ export class InputManager {
     keyHandler: Map<string, () => void> = new Map();
     keyStates: Set<string> = new Set();
     mouseStates: Set<number> = new Set();
+    gamepads: Gamepad[] = [];
+    buttonCooldown: Map<number, number> = new Map();
 
     constructor(game: Game) {
         this.game = game;
@@ -79,7 +81,8 @@ export class InputManager {
     }
 
     update() {
-        const movement = { x: 0, y: 0, z: 0 };
+        const movement = { x: 0, y: 0, z: 0, sneak: false };
+        const actions = { dig: false, place: false };
 
         if (this.keyStates.has('KeyQ')) {
             this.game.player.dropItem();
@@ -105,16 +108,98 @@ export class InputManager {
         if (this.keyStates.has('Space')) {
             movement.y = 1;
         }
+        if(this.keyStates.has('ShiftLeft')) {
+            movement.sneak = true;
+        }
+
         const player = this.game.player;
+
+        for(const gamepad of navigator.getGamepads()){
+            if(gamepad){
+                if(gamepad.axes.length >= 2){
+                    const len = Math.sqrt(gamepad.axes[0] * gamepad.axes[0]  + gamepad.axes[1] * gamepad.axes[1]);
+                    if(len > 0.16){
+                        movement.x = gamepad.axes[0];
+                        movement.z = gamepad.axes[1];
+                    }
+                }
+                if(gamepad.axes.length >= 4){
+                    const len = Math.sqrt(gamepad.axes[2] * gamepad.axes[2]  + gamepad.axes[3] * gamepad.axes[3]);
+                    if(len > 0.16){
+                        player.rotate(gamepad.axes[2] * -0.01, gamepad.axes[3] * -0.01);
+                    }
+                }
+                if(gamepad.buttons[0]?.pressed){
+                    movement.y = 1;
+                }
+                if(gamepad.buttons[2]?.pressed){
+                    movement.y = -1;
+                }
+                if(gamepad.buttons[3]?.pressed){
+                    movement.sneak = true;
+                }
+                if(gamepad.buttons[4]?.pressed){
+                    const key = gamepad.index | (4 << 8);
+                    const cooldown = this.buttonCooldown.get(key) || 0;
+                    if(cooldown < this.game.ticks){
+                        const newSelection = (player.inventory.selection - 1) % player.inventory.items.length;
+                        player.inventory.select(
+                            newSelection >= 0
+                                ? newSelection
+                                : player.inventory.items.length - newSelection - 2
+                        );
+                        this.buttonCooldown.set(key, this.game.ticks + 5);
+                    }
+                } else {
+                    const key = gamepad.index | (4 << 8);
+                    this.buttonCooldown.delete(key);
+                }
+                if(gamepad.buttons[5]?.pressed){
+                    const key = gamepad.index | (5 << 8);
+                    const cooldown = this.buttonCooldown.get(key) || 0;
+                    if(cooldown < this.game.ticks){
+                        const newSelection = (player.inventory.selection + 1) % player.inventory.items.length;
+                        player.inventory.select(
+                            newSelection >= 0
+                                ? newSelection
+                                : player.inventory.items.length - newSelection - 2
+                        );
+                        this.buttonCooldown.set(key, this.game.ticks + 5);
+                    }
+                } else {
+                    const key = gamepad.index | (5 << 8);
+                    this.buttonCooldown.delete(key);
+                }
+                if(gamepad.buttons[8]?.pressed){
+                    player.noClip = true;
+                }
+                if(gamepad.buttons[9]?.pressed){
+                    player.noClip = false;
+                }
+
+                if(gamepad.buttons[7]?.pressed){
+                    if(!gamepad.buttons[7].value || gamepad.buttons[7].value > 0.5){
+                        actions.dig = true;
+                    }
+                }
+                if(gamepad.buttons[6]?.pressed){
+                    if(!gamepad.buttons[6].value || gamepad.buttons[6].value > 0.5){
+                        actions.place = true;
+                    }
+                }
+                //console.log(gamepad.buttons.map((b,i) => b.pressed ? `${i}: ${b.value}` : '').join(' '));
+            }
+        }
+
         if (player.noClip) {
-            const speed = this.keyStates.has('ShiftLeft') ? 1.5 : 0.3;
+            const speed = movement.sneak ? 1.5 : 0.3;
             player.fly(
                 movement.x * speed,
                 movement.y * speed,
                 movement.z * speed
             );
         } else {
-            const speed = this.keyStates.has('ShiftLeft') ? 0.05 : 0.2;
+            const speed = movement.sneak ? 0.05 : 0.2;
             player.move(
                 movement.x * speed,
                 movement.y * speed,
@@ -124,10 +209,18 @@ export class InputManager {
 
         this.game.player.miningActive = false;
         if (this.mouseStates.has(0)) {
-            this.game.player.dig();
+            actions.dig = true;
         }
 
         if (this.mouseStates.has(2)) {
+            actions.place = true;
+
+        }
+
+        if(actions.dig){
+            this.game.player.dig();
+        }
+        if(actions.place){
             this.game.player.useItem();
         }
     }
