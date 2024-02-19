d5da68f50b0767ef4c7ebd2eba176671cba82eaf
Benjamin Vincent Schulenburg <benjamin.schulenburg@institut-fuer-digitales-lernen.de>
2024-02-19 17:24:16 +0100

    Minor updates/refactoring

--- src/server/connection.ts
+++ src/server/connection.ts
@@ -4,54 +4,11 @@
 import { WebSocket } from 'ws';
 import type { Server } from './server';
 import type {
-	WSChatMessage,
 	WSHelloMessage,
 	WSMessage,
-	WSNameChange,
 	WSPlayerUpdate,
 } from '../network';
-
-const handler: Map<string, (con: ClientConnection, msg: WSMessage) => void> =
-	new Map();
-export const addHandler = (
-	T: string,
-	fun: (con: ClientConnection, msg: WSMessage) => void
-) => handler.set(T, fun);
-
-handler.set('msg', (con, raw) => {
-	const msg = raw as WSChatMessage;
-	console.log(`Chat: ${msg.msg}`);
-	con.server.sendAll(raw);
-});
-
-handler.set('nameChange', (con, raw) => {
-	const msg = raw as WSNameChange;
-	con.playerName = msg.newName;
-	con.server.sendAll({
-		T: 'msg',
-		msg: `${con.playerName} joined`,
-	} as WSChatMessage);
-});
-
-handler.set('playerUpdate', (con, raw) => {
-	const msg = raw as WSPlayerUpdate;
-	con.x = msg.x;
-	con.y = msg.y;
-	con.z = msg.z;
-
-	con.yaw = msg.yaw;
-	con.pitch = msg.pitch;
-
-	con.health = msg.health;
-	con.maxHealth = msg.maxHealth;
-
-	for (const ccon of con.server.sockets.values()) {
-		if (ccon.id === con.id) {
-			continue;
-		}
-		con.send(ccon.getPlayerUpdateMsg());
-	}
-});
+import { handler } from './handler';
 
 let idCounter = 0;
 export class ClientConnection {
