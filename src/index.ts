b4359c05c2e584f939ef85b28e90297a298f5fc9
Benjamin Vincent Schulenburg <benjamin.schulenburg@institut-fuer-digitales-lernen.de>
2024-04-05 17:32:44 +0200

    Some reorganization

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+/* Copyright 2023 - Benjamin Vincent Schulenburg
+ * Licensed under the AGPL3+, for the full text see /LICENSE
+ */
+import { Game } from './game';
+import { ClientGame } from './client/clientGame';
+import './style.css';
+
+declare global {
+	interface Window {
+		wow: ClientGame;
+	}
+}
+
+const main = () => {
+	const parent = document.querySelector('#wolkenwelten-parent');
+	if (!parent) {
+		throw new Error("Can't find WolkenWelten parent element");
+	}
+	const game = new Game({
+		parent: parent as HTMLElement,
+	});
+	const clientGame = new ClientGame(game);
+	window.wow = clientGame;
+	console.log(
+		"%cHello there,%c most of the game state is accessible as the 'wow' object, happy hacking! :3",
+		'color:green',
+		'color:inherit'
+	);
+};
+setTimeout(main, 0);
